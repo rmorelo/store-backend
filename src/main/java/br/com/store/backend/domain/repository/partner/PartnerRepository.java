801a6e18c0134b32f2f4753b0dd3230795c424e1
rmorelo <rafael_morelo@yahoo.com.br>
2015-10-29 23:46:30 -0200

    ajuste no repository

--- src/main/java/br/com/store/backend/domain/repository/partner/PartnerRepository.java
+++ src/main/java/br/com/store/backend/domain/repository/partner/PartnerRepository.java
@@ -1,15 +1,11 @@
 package br.com.store.backend.domain.repository.partner;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import br.com.store.backend.domain.entity.PartnerEntity;
 import br.com.store.backend.infrastructure.rest.RestClientException;
-import br.com.uol.cubus.support.domain.entity.question.SecurityUserQuestionEntity;
-import br.com.uol.cubus.support.domain.repository.question.JpaRepository;
 
-public interface PartnerRepository extends JpaRepository<SecurityUserQuestionEntity, Long>{
-
-    String SCHEME = "http";
-    String HOST = "store.backend.intranet";
-    int PORT = 443;
+public interface PartnerRepository extends JpaRepository<PartnerEntity, Long>{
 
     PartnerEntity getPartner(Long idPartner) throws RestClientException;
 
